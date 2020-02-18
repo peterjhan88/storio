22f31e8c64d2db3083f54a71b41d1e3aa5db6a7a
Peter Han <peterjhan88@gmail.com>
2020-02-18 15:52:55 -0800

    Conversations.jsx changed into functional component

--- client/components/Conversations.jsx
+++ client/components/Conversations.jsx
@@ -1,45 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { withRouter, Link } from 'react-router-dom';
 
-class Conversations extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      correspondents: []
-    };
-  }
+import { UserContext } from './UserContext';
 
-  componentDidMount() {
-    fetch(`/api/conversations/signedInUserId/${this.props.match.params.loggedInUserId}`)
+const Conversations = props => {
+  const [correspondents, setCorrespondents] = useState([]);
+
+  const [currentUser] = useContext(UserContext);
+
+  useEffect(() => {
+    fetch(`/api/conversations/signedInUserId/${currentUser.userId}`)
       .then(response => response.json())
-      .then(jsonData => this.setState({ correspondents: jsonData }))
+      .then(jsonData => setCorrespondents(jsonData))
       .catch(err => console.error(err));
-  }
+  }, []);
 
-  render() {
-    const others = this.state.correspondents.map(otherPerson => {
-      return (
-        <Link key={otherPerson.userId} to={`/message/${this.props.match.params.loggedInUserId}/${otherPerson.userId}`}>
-          <div className='col-10 mx-auto my-1 conversation-list-single-user'>
-            <div className='conversation-list-profile-picture-container'>
-              <img
-                src={otherPerson.profilePicturePath}
-                alt={`Picture of userId=${otherPerson.userId} should be here`}
-                className='conversation-list-profile-picture'
-              />
-            </div>
-            <div className='conversation-list-profile-name'>{`${otherPerson.firstName} ${otherPerson.lastName}`}</div>
-          </div>
-        </Link>
-      );
-    });
+  const others = correspondents.map(otherPerson => {
     return (
-      <>
-        <p className="text-muted" onClick={() => this.props.history.goBack()}>&lt; Back</p>
-        <div className="d-flex flex-column conv">{others.length === 0 ? <h3>No conversations to list</h3> : others}</div>
-      </>
+      <Link key={otherPerson.userId} to={`/message/${currentUser.userId}/${otherPerson.userId}`}>
+        <div className='col-10 mx-auto my-1 conversation-list-single-user'>
+          <div className='conversation-list-profile-picture-container'>
+            <img
+              src={otherPerson.profilePicturePath}
+              alt={`Picture of userId=${otherPerson.userId} should be here`}
+              className='conversation-list-profile-picture'
+            />
+          </div>
+          <div className='conversation-list-profile-name'>{`${otherPerson.firstName} ${otherPerson.lastName}`}</div>
+        </div>
+      </Link>
     );
-  }
-}
+  });
+  return (
+    <>
+      <p className="text-muted" onClick={() => props.history.goBack()}>&lt; Back</p>
+      <div className="d-flex flex-column conv">{others.length === 0 ? <h3>No conversations to list</h3> : others}</div>
+    </>
+  );
+};
 
 export default withRouter(Conversations);
